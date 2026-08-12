6a3623a235214172dfe3fb5946817de5e30848b9
Tamanna Dagar <daga.tamanna.dagar@gmail.com>
2026-08-12 21:38:53 +0530

    created the pipeline to search the papers from the topic and give the papers and saved the papers successfully!

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,19 @@
-print("AutoResearch- AI Started")
+from paper_search import search_papers
+import json
+
+topic= "Generative AI in Education"
+
+papers= search_papers(topic, 10)
+
+with open ('data/papers/search_results.json', 'w', encoding='utf-8' ) as file:
+    json.dump(papers, file, indent=4, ensure_ascii=False)
+
+print(f"Found {len(papers)} papers")
+print("Result saved successfully!")
+
+
+for paper in papers:
+    print("\n Title:", paper['title'] )
+    print('Year:', paper['year'])
+    print('DOI:', paper['doi'])
+    print("Citations:", paper['cited_by_count'])
---
+++ backend/paper_search.py
@@ -0,0 +1,31 @@
+import requests
+
+def search_papers(topic, per_page=10):
+    url= " https://api.openalex.org/works"
+
+    params= {
+        "search": topic,
+        "per_page": per_page
+    }
+
+    response= requests.get(url, params=params)
+
+    if response.status_code != 200:
+        print("Error While Seraching Paper")
+        return[]
+
+    data=response.json()
+    papers= []
+
+    for work in data['results']:
+        paper= {
+            "title": work.get("title"),
+            "year": work.get("publication_year"),
+            "doi": work.get("doi"),
+            "cited_by_count": work.get("cited_by_count"),
+            "openalex_id": work.get("id")
+        }
+
+        papers.append(paper)
+
+    return papers
